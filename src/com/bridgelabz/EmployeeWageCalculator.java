2a68455d38fb2a3f4e65a2d5ef2bd914774d1deb
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 17:46:41 +0530

    MODIFY: Create Attendance Monitor

--- src/com/bridgelabz/EmployeeWageCalculator.java
+++ src/com/bridgelabz/EmployeeWageCalculator.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!");
+		int fullTime = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == fullTime)
+		System.out.println("Employee is Present");
+		else
+		System.out.println("Employee is Absent");
 	}
 }
