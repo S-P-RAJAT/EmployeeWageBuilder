8282be5a76fb67f9e6d42cff677d377117e7ce36
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 17:52:03 +0530

    MODIFY: Add Part-time work details

--- src/com/bridgelabz/EmployeeWageCalculator.java
+++ src/com/bridgelabz/EmployeeWageCalculator.java
@@ -4,12 +4,15 @@ public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!");
 		int fullTime = 1;
+		int partTime = 2;
 		int wagePerHour = 20;
 		int empHours = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == fullTime)
 			empHours = 8;
+		else if (empCheck == partTime)
+			empHours = 4;
 		else
 			empHours = 0;
 		empWage = empHours * wagePerHour;
