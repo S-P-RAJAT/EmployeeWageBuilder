32a48eb269aa998da345e73c0e609972097b5eeb
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 17:57:40 +0530

    MODIFY: Compute Wages for a month or max hours

--- src/com/bridgelabz/EmployeeWageCalculator.java
+++ src/com/bridgelabz/EmployeeWageCalculator.java
@@ -5,27 +5,28 @@ public class EmployeeWageCalculator {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!");
-		int empHours = 0, empWage = 0,totalEmpWage=0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS;day++)
-		{
-		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case IS_FULL_TIME:
-			empHours = 8;
-			break;
-		case IS_PART_TIME:
-			empHours = 4;
-			break;
-		default:
-		empHours = 0;
+		int empHours = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHours = 8;
+				break;
+			case IS_PART_TIME:
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
+			}
+			totalEmpHrs += empHours;
+			System.out.println("Day " + totalWorkingDays + " - Hours worked: " + empHours);
 		}
-		empWage = empHours * EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage;
-		System.out.println("Emp Wage:" + empWage) ;
-		}
-		System.out.println("Total Emp Wagefor a Month:" + totalEmpWage);
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
